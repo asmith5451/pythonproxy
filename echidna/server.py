6bfda2bd76bd8d410fa304e06b787c7502d23d9e
Camron Lent <camronlent@gmail.com>
2016-03-29 14:23:54 -0700

    Simplify server class

--- echidna/server.py
+++ echidna/server.py
@@ -35,29 +35,9 @@ class ProxyServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
         host, port = server_address
         host = socket.gethostbyname(host)
         server_address = (host, port)
-        super(ProxyServer, self).__init__(server_address, handler_class)
+        super().__init__(server_address, handler_class)
         self.dest_address = dest_address
         return
-    def server_activate(self):
-        super(ProxyServer, self).server_activate()
-        return
-    def serve_forever(self, poll_interval=0.5):
-        super(ProxyServer, self).serve_forever(poll_interval)
-        return
-    def handle_request(self):
-        return super(ProxyServer, self).handle_request()
-    def verify_request(self, request, client_address):
-        return super(ProxyServer, self).verify_request(request, client_address)
-    def process_request(self, request, client_address):
-        return super(ProxyServer, self).process_request(request, client_address)
-    def server_close(self):
-        return super(ProxyServer, self).server_close()
-    def finish_request(self, request, client_address):
-        return super(ProxyServer, self).finish_request(request, client_address)
-    def close_request(self, request_address):
-        return super(ProxyServer, self).close_request(request_address)
-    def shutdown(self):
-        return super(ProxyServer, self).shutdown()
 
 if __name__ == '__main__':
     setup()
